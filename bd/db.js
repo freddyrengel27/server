12b2d7ddc90065c25ac66b5c8261161d96e43870
freddyrengel27 <freddy27rengel@gmail.com>
2022-08-12 12:50:39 -0400

    actualizando port db

--- bd/db.js
+++ bd/db.js
@@ -7,7 +7,8 @@ const configPool = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATATABLES
+    database: process.env.DB_DATATABLES,
+    port: process.env.DB_PORT
 };
 
 
